673dd27a897630fde9b3b97b649a1fb93c3f3ea3
Orest Mirko <orestmirko@gmail.com>
2024-12-12 01:01:50 +0200

    update password dto

--- src/dtos/admin/change-password.dto.ts
+++ src/dtos/admin/change-password.dto.ts
@@ -1,14 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, Length } from 'class-validator';
+import { IsString, MaxLength, NotContains, IsStrongPassword, Matches } from 'class-validator';
+
+const SPECIAL_CHARS_REGEX_FOR_PASSWORD = /[!@#$%^&*(),.?":{}|<>]/;
 
 export class ChangePasswordDto {
-  @ApiProperty()
+  @ApiProperty({
+    example: 'YourOldPassword123!',
+  })
   @IsString()
-  @Length(8, 30)
   oldPassword: string;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'Qwerty123!',
+  })
   @IsString()
-  @Length(8, 30)
+  @MaxLength(20)
+  @NotContains(' ', { message: 'Password should not contain spaces' })
+  @IsStrongPassword(
+    {
+      minLength: 8,
+      minLowercase: 1,
+      minUppercase: 1,
+      minNumbers: 1,
+      minSymbols: 1,
+    },
+    {
+      message:
+        'Password must contain at least 8 characters, one uppercase letter, one lowercase letter, one number and one special character',
+    },
+  )
+  @Matches(SPECIAL_CHARS_REGEX_FOR_PASSWORD, {
+    message: 'Password must contain at least one special character (e.g., !, @, #, $, etc.)',
+  })
   newPassword: string;
 }
